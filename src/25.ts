2b138cdf8ca702aa017567f30dd2d54b798884ed
matheussmith469 <matheussmith469@gmail.com>
2025-04-05 04:59:56 +0000

    Add 25.ts

---
+++ src/25.ts
@@ -0,0 +1,5 @@
+// This is a sample TypeScript code.
+// It demonstrates how to create a simple program with basic operations and variables.
+let age: number = 20;
+let favoriteColor: string = "purple";
+console.log(`My current age is ${age} and my favorite color is ${favoriteColor}.`);
